7eac61c7adc960aff97f44183d249f645f64e568
RH90 <hasanaj11@hotmail.com>
2022-01-15 23:42:12 +0100

    2022-01-15_2342

--- Replay.user.js
+++ Replay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replay
 // @namespace    http://tampermonkey.net/
-// @version      19.0
+// @version      19.1
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.youtube.com/*
@@ -272,6 +272,8 @@ function getReplayStatus(videoID) {
 					}
 					sliderStart.value = element.start;
 					sliderStop.value = element.stop;
+					console.log("element.start");
+					console.log(element.start);
 				}
 				replayCheckChange(true);
 				saveReplayLinks();
