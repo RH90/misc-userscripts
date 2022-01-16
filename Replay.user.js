d39e8b63223b8ed79f61a294b55dc00c3557702d
RH90 <hasanaj11@hotmail.com>
2022-01-16 01:40:10 +0100

    2022-01-16_0140

--- Replay.user.js
+++ Replay.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name		Replay
-// @namespace	http://tampermonkey.net/
-// @version		20.4
-// @description	try to take over the world!
-// @author		You
-// @match		*://www.youtube.com/*
-// @grant		none
-// @run-at		document-end
-// @downloadURL	https://github.com/RH90/misc-userscripts/blob/master/Replay.user.js?raw=true
+// @name         Replay
+// @namespace    http://tampermonkey.net/
+// @version      20.5
+// @description  try to take over the world!
+// @author       You
+// @match        *://www.youtube.com/*
+// @grant        none
+// @run-at       document-end
+// @downloadURL  https://github.com/RH90/misc-userscripts/blob/master/Replay.user.js?raw=true
 // @noframes
 // ==/UserScript==
 var yt;
