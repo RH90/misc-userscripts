9582788163841b6eca980339923b3a0caca48c6b
RH90 <hasanaj11@hotmail.com>
2022-06-15 21:25:46 +0200

    2022-06-15_2125

--- Replay.user.js
+++ Replay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replay
 // @namespace    http://tampermonkey.net/
-// @version      21.1
+// @version      21.2
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.youtube.com/*
@@ -456,6 +456,8 @@ function saveReplayLinks() {
 		object.id = linkTest;
 		object.start = sliderStart.value;
 		object.stop = sliderStop.value;
+		object.updated = new Date().toLocaleString();
+
 		console.log(object);
 		arr.push(object);
 
